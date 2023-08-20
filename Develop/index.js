cea6ba37e9a9c41c9fe37781ef0ba350220d8fb0
Kevin-Stephens <kmstephens437@gmail.com>
2023-08-20 18:07:04 -0500

    created all content for quetions and write file.

--- Develop/index.js
+++ Develop/index.js
@@ -1,18 +1,71 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer')
-const fs = reuire('fs')
+const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
-const questions = [{
-    
-}
-];
+const questions = () => inquirer.prompt ([
+    {
+        name: 'title',
+        type: 'input',
+        message: 'What is the Title of your project?'
+    },
+    {
+        name: 'description',
+        type: 'input',
+        message: 'Please decribe your project here.'
+    },
+    {
+        name: 'tableOfContents',
+        type: 'input',
+        message: 'Please enter your table of contents here.'
+    },
+    {
+        name: 'installation',
+        type: 'input',
+        message: 'Please enter installation instructions here.'
+    },
+    {
+        name: 'usage',
+        type: 'input',
+        message: 'Please enter usage information here.'
+    },
+    {
+        name: 'license',
+        type: 'input',
+        message: 'Please enter the license name here.'
+    },
+    {
+        name: 'contribution',
+        type: 'input',
+        message: 'Please enter contribution guidelines here.'
+    },
+    {
+        name: 'testing',
+        type: 'input',
+        message: 'Please enter instructions for testing here.'
+    },
+    {
+        name: 'questions',
+        type: 'input',
+        message: 'Please enter contact method(s) for questions here.'
+    }
+]);
+
+questions()
+    .then((answers) => {
+        const generated = generateMarkdown(answers);
+        fs.writeFileSync ('README.md',generated)
+    })
+    .then(() => console.log('Success! README file created'))
+    .catch(err => console.log(err));
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+//function init() {}
 
 // Function call to initialize app
-init();
+//init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,43 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+//function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+//function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+//function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answers) {
+  return `  # ${answers.title}
+
+  ## Description
+  ${answers.description}
+
+  ## Table of Contents
+  ${answers.tableOfContents}
+
+  ## Installation Instructions
+  ${answers.installation}
+
+  ## Usage Information
+  ${answers.usage}
+
+  ## License
+  ${answers.license}
+
+  ## Contribution Guidelines
+  ${answers.contribution}
+
+  ## Testing
+  ${answers.testing}
+
+  ## Questions
+  ${answers.questions}
+
 
 `;
 }
